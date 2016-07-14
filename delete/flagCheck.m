067784e69728be83637b7ad3bbe9a375cb828f75
Lu <sino.lv@gmail.com>
2016-07-14 11:19:06 -0700

    delete branch delete

--- delete/flagCheck.m
+++
@@ -1,8 +0,0 @@
-function [variableStructure] = flagCheck(variableStructure)
-A =  exist([variableStructure.abortingPath,'\','SMDAbort.txt'], 'file');
-
-if A ==2
-    variableStructure.flag = 1; 
-    %delete([data_savePath,'\','Aboritng.mat']);
-    
-end
